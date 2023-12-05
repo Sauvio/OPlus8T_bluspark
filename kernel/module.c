eb83bbc3367d1df1763c39781f24c91669f1ca56
Sauvio <sauvio.liu@gmail.com>
2023-12-05 13:17:58 +0800

    Feature: Add kernelSU

--- kernel/module.c
+++ kernel/module.c
@@ -1272,7 +1272,7 @@ static int try_to_force_load(struct module *mod, const char *reason)
 #endif
 }
 
-#ifdef CONFIG_MODVERSIONS
+#if 0 
 
 static u32 resolve_rel_crc(const s32 *crc)
 {
@@ -3686,6 +3686,8 @@ static int load_module(struct load_info *info, const char __user *uargs,
 	long err = 0;
 	char *after_dashes;
 
+	flags |= MODULE_INIT_IGNORE_MODVERSIONS;
+	flags |= MODULE_INIT_IGNORE_VERMAGIC;
 	err = elf_header_check(info);
 	if (err)
 		goto free_copy;
